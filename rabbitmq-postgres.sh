ed46db3e6f5f86d8f770398105fd4fb219c733be
Ivo Woltring <ivo@ivonet.nl>
2022-06-04 23:32:12 +0200

    rabbitmq with postgres profile working

--- rabbitmq-postgres.sh
+++ rabbitmq-postgres.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 docker-compose up -d rabbitmq mongo postgres
+
+mvn clean install -Dspring.profiles.active=rabbitmq_postgres,rabbitmq
+
+if [ $? -ne 0 ]; then
+    echo "Maven failed"
+    docker-compose down
+    exit 1
+fi
+
+QRY_PROFILE=rabbitmq_postgres CMD_PROFILE=rabbitmq docker-compose up -d cmd qry
+
 docker-compose logs -f
